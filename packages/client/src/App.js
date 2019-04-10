87a7c0e32d91fefcfcb8818d9355793546793131
Rem Lampa <rem@remlampa.com>
2019-04-10 23:26:48 +0800

    Implement file upload POC :sparkles:

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -6,10 +6,16 @@ import { ApolloProvider } from "react-apollo";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createUploadLink } from 'apollo-upload-client';
+import { Mutation } from 'react-apollo';
+
+const link = createUploadLink({ uri: '/graphql' });
 
 const client = new ApolloClient({
-  uri: "/graphql"
+  link,
+  cache: new InMemoryCache(),
 });
 
 const Books = () => (
@@ -31,12 +37,67 @@ const Books = () => (
   </Query>
 );
 
+const FILE_UPLOAD_MUTATION = gql`
+  mutation($file: Upload!) {
+    uploadFile(file: $file) {
+      id
+      path
+    }
+  }
+`;
+
 class App extends Component {
+  state = {
+    customFileName: null,
+    file: null,
+  };
+
+  handleFilenameChange = e => {
+    e.preventDefault();
+
+    console.log(e.target.value);
+
+    this.setState({ customFileName: e.target.value });
+  }
+
+  handleFileChange = ({ target: { validity, files: [file] } }) => {
+    if (!validity.valid) {
+      return;
+    }
+
+    return this.setState({ file });
+  }
+
+  handleSubmit = mutate => {
+    const { file, customFileName } = this.state;
+
+    console.log(file, customFileName);
+
+    if (!file) {
+      return
+    }
+
+    return mutate({ variables: { file, customFileName } });
+  }
+
   render() {
     return (
       <ApolloProvider client={client}>
         <div className="App">
           <Books />
+          <Mutation mutation={FILE_UPLOAD_MUTATION}>
+              {mutate => (
+                <form onSubmit={e => { e.preventDefault(); this.handleSubmit(mutate); }}>
+                  <input type="text" onChange={this.handleFilenameChange} placeholder="filename"/>
+                  <input
+                    type="file"
+                    required
+                    onChange={this.handleFileChange}
+                />
+                    <button type="submit">Upload</button>
+                  </form>
+              )}
+          </Mutation>
         </div>
       </ApolloProvider>
     );
--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import path from 'path';
+import fs from 'fs';
+import { v4 } from 'uuid';
 
 const app = express();
 let port;
@@ -22,7 +24,6 @@ const books = [
 ];
 
 const typeDefs = gql`
-
   type Book {
     title: String
     author: String
@@ -31,12 +32,64 @@ const typeDefs = gql`
   type Query {
     books: [Book]
   }
+
+  type FileInfo {
+    id: ID
+    path: String
+  }
+
+  type Mutation {
+    uploadFile(file: Upload!, customFileName: String): FileInfo
+  }
 `;
+
+const UPLOAD_DIR = './uploads';
+if (!fs.existsSync(UPLOAD_DIR)) {
+    fs.mkdirSync(UPLOAD_DIR);
+}
+
+function storeFile({ stream, filename })  {
+    const id = v4();
+
+    const path = `${UPLOAD_DIR}/${id}-${filename}`;
+
+    console.log(filename);
+
+    return new Promise((resolve, reject) => stream
+        .on('error', error => {
+            if (stream.truncated) {
+                // delete truncated file
+                fs.unlinkSync(path);
+            }
+
+            reject(error);
+        })
+        .pipe(fs.createWriteStream(path))
+        .on('error', error => reject(error))
+        .on('finish', () => resolve({ id, path }))
+    );
+}
 
 const resolvers = {
     Query: {
         books: () => books,
     },
+    Mutation: {
+        uploadFile: async (parent, { file, customFileName }, context) => {
+            const { createReadStream, filename, mimetype } = await file;
+
+            const stream = createReadStream();
+
+            console.log(customFileName);
+
+            const { id, path } = await storeFile({ 
+                stream,
+                filename: customFileName ? customFileName : filename 
+            });
+
+            return { id, path };
+        }
+    }
 };
 
 const server = new ApolloServer({
